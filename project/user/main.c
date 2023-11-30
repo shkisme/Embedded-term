8b0dc287bca338258ca93fe75859f6387a7a123b
손현경 <shkisme0130@gmail.com>
2023-11-30 22:28:03 +0900

    Update main.c

--- project/user/main.c
+++ project/user/main.c
@@ -7,9 +7,10 @@
 #include "stm32f10x_usart.h"
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_adc.h"
+#include "stm32f10x_tim.h"
 #include "lcd.h"
 #include "touch.h"
-#include "stm32f10x_tim.h"
+
 
 /* function prototype */
 void RCC_Configure(void);
@@ -28,7 +29,7 @@ void TIM2_IRQHandler(void);
 
 
 uint16_t value, x, y, time_count;
-bool isspin = false;
+int isspin, led;
 int color[12] = {WHITE, CYAN, BLUE, RED, MAGENTA, LGRAY, GREEN, YELLOW, BROWN, BRRED, GRAY};
 
 // Servo Motor variable
@@ -38,11 +39,12 @@ uint16_t curDeg = 700, minDeg = 700, maxDeg = 2600;
 void RCC_Configure(void)
 {
     // USART2 : PA2, PA3
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
     
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE); // 아날로그 센서 1 : PC2
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);  // ADC1 ENABLE
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
@@ -55,12 +57,12 @@ void GPIO_Configure(void)
     GPIO_InitTypeDef GPIO_InitStructure;
 
     //Servo Motor part
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;  // Enable Servo Motor : PB0
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; // Enable Servo Motor : PB0
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     // ADC_1 Part
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;   // 아날로그 센서 1 : PC2
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;     // 아날로그 센서 1 : PC2
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN; // PC2 ->  ADC (Input)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
     GPIO_Init(GPIOC, &GPIO_InitStructure);
@@ -74,7 +76,7 @@ void GPIO_Configure(void)
      // Motor Part
      // 릴레이 모듈 등 여러가지를 연결하려면 번거롭기 때문에 불가피하게 LED의 전원 상태로
      // 전원이 정상적으로 인가되었는지 확인
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2; // Enable Motor : PD2(나중에 OR을 통해 2개까지 조작 가능)
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3; // Enable Motor : PD2, PD3(나중에 OR을 통해 2개까지 조작 가능)
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
@@ -85,7 +87,7 @@ void GPIO_Configure(void)
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; // 50Mhz
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // Alternate function
 	GPIO_Init(GPIOA, &GPIO_InitStructure); // PA2
-	//RX
+    //RX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3; // Rx -> PA3
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; // 50Mhz
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; // Pull-Up
@@ -191,80 +193,78 @@ void NVIC_Configure(void) {
     NVIC_InitTypeDef NVIC_InitStructure;
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 
+    // Button S1(PC4)
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI4_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
     // Timer 2
     NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
 	NVIC_Init(&NVIC_InitStructure);
 
     // Timer 3
 	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
 	NVIC_Init(&NVIC_InitStructure);
-
-    // Button S1(PC4)
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI4_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
 
     //ADC part
     NVIC_InitStructure.NVIC_IRQChannel = ADC1_2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
     NVIC_Init(&NVIC_InitStructure);
 
     // USART2
     // 'NVIC_EnableIRQ' is only required for USART setting
     NVIC_EnableIRQ(USART2_IRQn);
     NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3; // TODO
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 }
 
+// start of Handler
+/////////////////////////////////////////////////////
+
 void USART2_IRQHandler() {
     uint16_t word;
-    if(USART_GetITStatus(USART2,USART_IT_RXNE)!=RESET){
+    if(USART_GetITStatus(USART2,USART_IT_RXNE) != RESET){
         // the most recent received data by the USART2 peripheral
         word = USART_ReceiveData(USART2);
-
-        // TODO implement*********************
-        //LCD에 received message text를 띄움
-        LCD_ShowChar(80, 160, word, 20, BLACK, WHITE);
-
+        if(word == 'a'){
+          // TODO implement*********************
+          //LCD에 received message text를 띄움
+          LCD_ShowChar(80, 160, ' ', 20, BLACK, WHITE);
+          LCD_ShowString(80, 160, "start", BLACK, WHITE);
+        }
         // clear 'Read data register not empty' flag
     	USART_ClearITPendingBit(USART2,USART_IT_RXNE);
     }
+    USART_ClearITPendingBit(USART2,USART_IT_RXNE);
 }
 
-void EXTI4_IRQHandler(void){
+void EXTI4_IRQHandler(void){ /////////w35879083759279047204723907420
     if(EXTI_GetITStatus(EXTI_Line4) != RESET) { // KEY1
+      
         if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_4) == Bit_RESET) {
-
             //모터가 켜져있는 상태에서는 꺼지고 꺼져있는 상태에서는 꺼짐
-            if(isspin){ // on -> off
-                GPIO_SetBits(GPIOD,GPIO_Pin_2);   // LED1 Off
-                LCD_ShowString(80, 100, "OFF", BLACK, WHITE);
-                isspin = false;
-            } else{     // off -> on
-                GPIO_ResetBits(GPIOD,GPIO_Pin_2); // LED1 On
-                LCD_ShowString(80, 100, " ON", BLACK, WHITE);
-                isspin = true;
-            }
-            
+          if(isspin == 1){
+            isspin = 0;
+        } else isspin = 1;
         }
         EXTI_ClearITPendingBit(EXTI_Line4);
     }
 }
 
-void ADC_Configure(void) {
+void ADC_Configure(void){
 	
     ADC_InitTypeDef ADC_12; // ADC12 채널 -> PC2
 	
@@ -289,11 +289,13 @@ void ADC_Configure(void) {
 }
 
 void ADC1_2_IRQHandler(void) {
-	
+// 디버깅으로 할 때는 정상적으로 동작하는데 
+  //디버깅 없이 할 때는 LCD가 동작하지 않음
+  //대체 뭐가 문제임
   if(ADC_GetITStatus(ADC1,ADC_IT_EOC) != RESET) { // ?�� ???? ??
 	  
-    value = ADC_GetConversionValue(ADC1); // ADC1?? ???? ???? value?? ???
-	
+    value = (int)ADC_GetConversionValue(ADC1); // ADC1?? ???? ???? value?? ???
+    
   }
   ADC_ClearITPendingBit(ADC1, ADC_IT_EOC);
   
@@ -302,55 +304,78 @@ void ADC1_2_IRQHandler(void) {
 void TIM2_IRQHandler() {
 	if(TIM_GetITStatus(TIM2,TIM_IT_Update) != RESET) { // ?�� ???? ??
 		
-		Servo_Change((uint16_t)curDeg);
+                if(led == 0) {
+                            GPIO_ResetBits(GPIOD,GPIO_Pin_3); // LED2 On
+                            led = 1;
+                } else {
+                            GPIO_SetBits(GPIOD,GPIO_Pin_3);   // LED2 Off
+                            led = 0;
+                    }
+          
 		if(isspin) { // DC ????? ??????????
-			
+                        Servo_Change((uint16_t)curDeg);
 			curDeg += 100;
 			if (curDeg > maxDeg) {
 				curDeg = minDeg;
 			}
-			time_count++;
-		    // 10?????
-		  	if(time_count % 10 ==0) {
-				time_count = 0;
-		    }
-		  
 		}
-        LCD_ShowNum(80,200, time_count, 3, BLACK, WHITE);
-
+                time_count++;
+                    if(time_count % 10 == 0) {
+			time_count = 0;
+		    }
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 	}
 }
 
-int main(void)
+/////////////////////////////////////////////////////
+//end of Handler
+
+ int main(void)
 {
 
     SystemInit();
     RCC_Configure();
     GPIO_Configure();
+    TIM_Configure();
+    EXTI_Configure();
     USART2_Init();
     ADC_Configure();
     NVIC_Configure();
+    
+    GPIO_SetBits(GPIOD,GPIO_Pin_7); 
+    GPIO_SetBits(GPIOD,GPIO_Pin_2); 
+    GPIO_SetBits(GPIOD,GPIO_Pin_3); 
     // --------------LCD Part----------------
     LCD_Init();
     Touch_Configuration();
-    Touch_Adjust();
+    //Touch_Adjust();
     LCD_Clear(WHITE);
 
+    LCD_ShowString(80,80,"THUR_Team01",BLACK,WHITE); // ?? ??? ???
     
-    LCD_ShowString(80,80,"THUR_Team11",BLACK,WHITE); // ?? ??? ???
-    LCD_ShowString(80, 100, "OFF", BLACK, WHITE);
     
     while (1) {
-      Touch_GetXY(&x, &y, 1); // ext?? 1?? ??????��? "??????" ????? 1???? ?????? ???
-	  								 // 0 ?????? ?????? ??? ?? ?? ?? ?? 2?? ???
-	  
-      Convert_Pos(x, y, &x, &y); // ??? ???
+      //Touch_GetXY(&x, &y, 1); // ext?? 1?? ??????��? "??????" ????? 1???? ?????? ???
+      //Convert_Pos(x, y, &x, &y); // ??? ???	
       
-	  if( x != 0 || y != 0 ) { // ????? ????? 0?? ????
+      if(isspin == 1){ // on -> off
+                //GPIO_ResetBits(GPIOD,GPIO_Pin_2);   // LED1 Off
+                LCD_ShowString(80, 100, "   ", BLACK, WHITE);
+                LCD_ShowString(80, 100, "ON", BLACK, WHITE);
+      } else{     // off -> on
+          //GPIO_SetBits(GPIOD,GPIO_Pin_2); // LED1 On
+          LCD_ShowString(80, 100, "   ", BLACK, WHITE);
+          LCD_ShowString(80, 100, "OFF", BLACK, WHITE);
+      }
+      LCD_ShowNum(120,200, time_count, 2, BLACK, WHITE);
+      LCD_ShowNum(80, 120, value / 64, 4, BLACK, WHITE); // (80, 120) ????? ???? ???? ?? ???
+	if( x != 0 || y != 0 ) { // ????? ????? 0?? ????
           LCD_DrawCircle(x, y, 3);							      // ??? ????? ?????? 3??? ?? ????
           LCD_ShowNum(80, 120, value, 4, BLACK, WHITE); // (80, 120) ????? ???? ???? ?? ???
+          
         }
+        
+        
       
     }
     return 0;
